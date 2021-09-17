5b0df1310432683463a87262258bcf5e3e6ef00f
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2021-09-17 12:31:28 +0100

    feat: Stripes v7 (#15)
    * feat: Stripes v7
    
    Upgraded deps to stripes 7.0.0
    
    * fix: peer-dep versions
    
    Fixed peer deps on react and react-dom to be ^17.0.2
    
    * refactor: route loading
    
    Changed from lazy loading routes to hard importing them like the rest of the ERM apps

---
+++ src/routes/index.js
@@ -0,0 +1,3 @@
+export { default as OARoute } from './OARoute';
+export { default as PublicationRequestRoute } from './PublicationRequestRoute';
+export { default as PublicationRequestCreateRoute } from './PublicationRequestCreateRoute';
