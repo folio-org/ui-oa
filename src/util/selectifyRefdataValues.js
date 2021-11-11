87c82029da5f99a428cb5b0e4cfc2c37fb807bfd
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-11-11 14:54:01 +0000

    refactor: Changed selectifyRefdataValues
    Tweaks to selectifyRefdata

--- src/util/selectifyRefdataValues.js
+++ src/util/selectifyRefdataValues.js
@@ -1,14 +1,14 @@
 
 // TODO this can probably be moved to stripes-kint-components
-const selectifyRefdata = (refdataRecords, category) => {
+const selectifyRefdata = (refdataRecords = [], category) => {
   if (category) {
     // If a category is provided then the incoming refdata will be a different shape
     return (
       refdataRecords
-        .filter(obj => obj.desc === category)
-        .map(obj => (
-          obj.values.map(entry => ({ label: entry.label, value: entry.id }))
-        ))[0]
+        .filter(obj => obj.desc === category)[0]
+        ?.values?.map(entry => (
+          { label: entry.label, value: entry.id }
+        )) ?? []
     );
   }
 
