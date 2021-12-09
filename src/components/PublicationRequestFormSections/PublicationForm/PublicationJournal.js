19810407e1b854a13bd08184592da3bea99ac543
Jack Golding <jack.golding@k-int.com>
2021-12-09 11:50:08 +0000

    feat: Added Typedown for Journals
    Added a typedown which allows for the selection of a journal from existing entries

--- src/components/PublicationRequestFormSections/PublicationForm/PublicationJournal.js
+++ src/components/PublicationRequestFormSections/PublicationForm/PublicationJournal.js
@@ -1,15 +1,12 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Field, useFormState, useForm } from 'react-final-form';
+import { Field, useFormState } from 'react-final-form';
 
 import {
   Col,
   Headline,
   Row,
-  Select,
-  TextField,
-  Checkbox,
 } from '@folio/stripes/components';
 import {
   generateKiwtQuery,
@@ -18,37 +15,19 @@ import {
 
 import { EditCard } from '@folio/stripes-erm-components';
 
-import useOARefdata from '../../../util/useOARefdata';
-import selectifyRefdata from '../../../util/selectifyRefdata';
 import JournalDetails from '../../PublicationRequestSections/JournalDetails';
-import PartyInfo from '../../PublicationRequestSections/PartyInfo';
 
 
 // @TODO
-// Placeholdsers for journal title etc. replace with title, ISSN Print and Elec.
-// Change path to endpoint for Journal Entries
-// Change onChange Function
-// useFormState?
-
-const [
-  OA_STATUS
-] = [
-  'PublicationRequest.OaStatus'
-];
+//Use correct path/endpoint to retrieve title,issn etc.
 
 const PublicationJournal = () => {
-  const refdataValues = useOARefdata([
-    OA_STATUS
-  ]);
-
-  const oaStatusValues = selectifyRefdata(refdataValues, OA_STATUS);
+  
   const { values } = useFormState();
-  const { change } = useForm();
-
+  
   const pathMutator = (input, path) => {
     const query = generateKiwtQuery(
-      // searchKey:"journalTitle"
-      { searchKey: 'familyName', stats: false },
+      { searchKey: 'journalTitle', stats: false },
       { query: input }
     );
     return `${path}${query}`;
@@ -57,11 +36,10 @@ const PublicationJournal = () => {
   const renderListItem = (journal) => {
     return (
       <>
-        {/* {journal.journalTitle} -
-        ISSN (Print): {journal.issnPrint} -
-        ISSN (Electronic): {journal.issnElectronic} -
-        OA Status: {journal.oaStatus} */}
-        {journal.title} - ISSN (Print): {journal.familyName} - ISSN (Electronic):{journal.givenNames}
+        Title: {journal.journalTitle} - 
+        ISSN (Print): {journal.issnPrint} - 
+        ISSN (Electronic): {journal.issnElectronic} - 
+        OA Status: {journal.oaStatus}
       </>
     );
   };
@@ -82,8 +60,7 @@ const PublicationJournal = () => {
             component={QueryTypedown}
             label={<FormattedMessage id="ui-oa.publicationRequest.journalTitle" />}
             name="selectJournal.journal"
-            // path="oa/titleInstances"
-            path="oa/party"
+            path="oa/titleInstances"
             pathMutator={pathMutator}
             renderListItem={renderListItem}
           />
@@ -91,11 +68,8 @@ const PublicationJournal = () => {
       </Row>
 
       {values.selectJournal?.journal &&
-        <EditCard
-          header={<FormattedMessage id="ui-oa.publicationRequest.selectedJournal" />}
-        >
-          {/* <JournalDetails request={} /> */}
-          <PartyInfo party={values.selectJournal.journal} />
+        <EditCard header={<FormattedMessage id="ui-oa.publicationRequest.selectedJournal" />}>
+          <JournalDetails request={values.selectJournal.journal} />
         </EditCard>
       }
     </>
