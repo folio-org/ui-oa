d4286cf5b1ccb59257a8b33a48b8f7ec38ee1886
Jack Golding <jack.golding@k-int.com>
2021-12-17 10:24:41 +0000

    chore: linting

--- src/components/PublicationRequestFormSections/index.js
+++ src/components/PublicationRequestFormSections/index.js
@@ -4,5 +4,5 @@ export { default as PublicationStatusForm } from './PublicationStatusForm';
 export { default as RequestInfoForm } from './RequestInfoForm';
 export { default as RequestContactForm } from './RequestContactForm';
 export { default as CorrespondingAuthorForm } from './CorrespondingAuthorForm';
-export { default as LinkAgreementForm } from './LinkAgreementForm'
+export { default as LinkAgreementForm } from './LinkAgreementForm';
 
