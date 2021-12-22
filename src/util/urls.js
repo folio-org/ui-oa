bda350df169ba2c9d58ab746c828de54223eac16
Jack Golding <jack.golding@k-int.com>
2021-12-22 11:35:29 +0000

    feat: Added correspondance urls
    Added correspondance urls for creating, editing and viewing

--- src/util/urls.js
+++ src/util/urls.js
@@ -2,7 +2,11 @@ const urls = {
   publicationRequests: () => '/oa/publicationRequests',
   publicationRequest: id => `/oa/publicationRequests/${id}`,
   publicationRequestCreate: () => '/oa/publicationRequests/create',
-  publicationRequestEdit: id => `/oa/publicationRequests/${id}/edit`
+  publicationRequestEdit: id => `/oa/publicationRequests/${id}/edit`,
+
+  publicationRequestCorrespondanceView: (prId, cId) => `/oa/publicationRequests/${prId}/correspondance/${cId}`,
+  publicationRequestCorrespondanceCreate: prId => `/oa/publicationRequests/${prId}/correspondance/create`,
+  publicationRequestCorrespondanceEdit: (prId, cId) => `/oa/publicationRequests/${prId}/correspondance/${cId}/edit`
 };
 
 export default urls;
