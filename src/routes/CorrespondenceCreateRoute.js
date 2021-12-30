ed9c18146de4511d6ca576bf1750a09071cc5828
Jack Golding <jack.golding@k-int.com>
2021-12-30 15:20:12 +0000

    feat: Added correspondence create route
    Changed correspondence create route to allow for the form to be correctly posted to oa/correspondence/

--- src/routes/CorrespondenceCreateRoute.js
+++ src/routes/CorrespondenceCreateRoute.js
@@ -2,36 +2,31 @@ import { Form } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 import { useHistory, useParams } from 'react-router-dom';
 import { useOkapiKy } from '@folio/stripes/core';
-import { useQuery, useMutation } from 'react-query';
+import { useMutation } from 'react-query';
 import CorrespondenceForm from '../components/views/CorrespondenceForm';
 
 const CorrespondenceCreateRoute = () => {
   const history = useHistory();
   const ky = useOkapiKy();
-  const { publicationRequestId } = useParams();
+  const { id } = useParams();
 
   const handleClose = () => {
-    history.push(`/oa/publicationRequests/${publicationRequestId}`);
+    history.push(`/oa/publicationRequests/${id}`);
   };
-
-  const { data: publicationRequest } = useQuery(
-    ['ui-oa', 'correspondenceCreateRoute', 'publicationRequest', publicationRequestId],
-    () => ky(`oa/publicationRequest/${publicationRequestId}`).json()
-  );
 
   const { mutateAsync: postCorrespondence } = useMutation(
     ['ui-oa', 'CorrespondenceCreateRoute', 'postCorrespondence'],
-    (data) => ky.put('/oa/correspondence/', { json: data }).then(() => {
+    (data) => ky.post('oa/correspondence', { json: data }).json().then(() => {
         handleClose();
       })
   );
-  const submitRequest = (values) => {
-    const submitValues = { ...values, 'owner':{ publicationRequest } };
+  const submitCorrespondence = (values) => {
+    const submitValues = { ...values, 'owner':{ id } };
     postCorrespondence(submitValues);
   };
 
   return (
-    <Form mutators={arrayMutators} onSubmit={submitRequest}>
+    <Form mutators={arrayMutators} onSubmit={submitCorrespondence}>
       {({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
           <CorrespondenceForm
