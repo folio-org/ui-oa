c44aaa977dc77e0a23d3e0d6894a319eb1e50618
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2022-01-12 15:41:39 +0000

    feat: Save link to agreement with request (#69)
    * feat: Changed agreement format
    
    Agreement field now saves agreement ID as remoteId
    
    * feat: Added editing of linked agreement
    
    Previously selected agreement now displayed correctly upon editing a page
    
    * feat: Added removal of linked agreement
    
    Linked agreement is now removed from publication request when "Request has no agreement" is selected
    
    * fix: Fixed agreement submission
    
    Agreement not submits correctly upon "No agreement" or changing agreement
    
    * chore: linting
    
    * chore: Readded _useCorrespondingAuthor

--- src/components/PublicationRequestFormSections/LinkAgreementForm/LinkAgreementForm.js
+++ src/components/PublicationRequestFormSections/LinkAgreementForm/LinkAgreementForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Field, useForm, useFormState } from 'react-final-form';
 
@@ -10,20 +10,29 @@ import {
   TextField,
 } from '@folio/stripes/components';
 
+import { get } from 'lodash';
 import { Registry } from '@folio/handler-stripes-registry';
 
 
 const LinkAgreementForm = () => {
-  const { values } = useFormState();
+  const { initialValues, values } = useFormState();
   const { change } = useForm();
-  const [agreement, setAgreement] = useState({});
 
   const resourceReg = Registry.getResource('agreement');
   const LookupComponent = resourceReg?.getLookupComponent() ?? TextField;
 
+  const initialAgreement = (get(initialValues, 'agreement.remoteId_object'));
+  const [agreement, setAgreement] = useState(initialAgreement ?? {});
+
+  useEffect(() => {
+    if (initialAgreement) {
+      setAgreement(initialAgreement);
+    }
+  }, [initialAgreement]);
+
   const handleAgreementSelected = (a) => {
     setAgreement(a);
-    change('agreement.name', a.id);
+    change('agreement.remoteId', a.id);
   };
 
   return (
@@ -41,7 +50,7 @@ const LinkAgreementForm = () => {
             onChange={(e) => {
               if (agreement) {
                 setAgreement({});
-                change('agreement.name', undefined);
+                change('agreement.remoteId', undefined);
               }
               change('noAgreement', e.target.checked);
             }}
@@ -51,7 +60,7 @@ const LinkAgreementForm = () => {
       {!values.noAgreement && (
         <Field
           component={LookupComponent}
-          name="agreement.name"
+          name="agreement.remoteId"
           onResourceSelected={handleAgreementSelected}
           resource={agreement}
         />
