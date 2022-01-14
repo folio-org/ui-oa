09ad0c4bf278e30726ea1ba667a7c606ca74c60b
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2022-01-14 16:58:26 +0000

    feat: Display Agreement in Request view pane (#73)
    * feat: Added basic framework
    
    Added basic framework for agreement in publication request
    
    * feat: no agreement checkbox wired up
    
    * feat: Basic Agreement accordion
    
    Added basic agreement accordion showing agreement card with agreement id used as placeholder, links to related agreement
    
    * feat: Formatted agreement card
    
    Formatted agreement card to show start/end date and status, included translations
    
    * feat: Conditional for rendering accordion
    
    Added conditional for rendering accordion assuming either agreement of explicitly without, also updated agreement card
    
    * feat: Render empty
    
    Added render empty to the agreement accordion and conditionals regarding what to render based on agreement and withoutAgreement
    
    * chore: linting
    
    * feat: Added registry
    
    Replaced url with registry link lookup
    
    * feat: Agreement name fallback
    
    Added agreement name fallback for OA standalone

---
+++ src/components/PublicationRequestSections/Agreement/Agreement.js
@@ -0,0 +1,118 @@
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+
+import {
+  Accordion,
+  Badge,
+  Card,
+  Row,
+  Col,
+  KeyValue,
+  NoValue,
+  FormattedUTCDate,
+  Layout,
+} from '@folio/stripes/components';
+import { AppIcon } from '@folio/stripes/core';
+import { Registry } from '@folio/handler-stripes-registry';
+
+import css from './Agreement.css';
+
+const propTypes = {
+  request: PropTypes.object,
+};
+
+const Agreement = ({ request }) => {
+  const resourceReg = Registry?.getResource('agreement');
+  const agreementLinkFunction = resourceReg?.getViewResource();
+
+  const renderBadge = (agreement) => {
+    return agreement ? <Badge>1</Badge> : <Badge>0</Badge>;
+  };
+
+  const renderEmpty = () => {
+    return (
+      <Layout className={css.agreementEmptyMessage}>
+        <FormattedMessage id="ui-oa.publicationRequest.agreementNotLinked" />
+      </Layout>
+    );
+  };
+
+  const renderAgreement = (agreement) => {
+    return (
+      <>
+        <Row>
+          <Col md={4} xs={6}>
+            <KeyValue
+              label={
+                <FormattedMessage id="ui-oa.publicationRequest.agreementStartDate" />
+              }
+              value={
+                agreement?.startDate ? (
+                  <FormattedUTCDate value={agreement.startDate} />
+                ) : (
+                  <NoValue />
+                )
+              }
+            />
+          </Col>
+          <Col md={4} xs={6}>
+            <KeyValue
+              label={
+                <FormattedMessage id="ui-oa.publicationRequest.agreementEndDate" />
+              }
+              value={
+                agreement?.endDate ? (
+                  <FormattedUTCDate value={agreement.endDate} />
+                ) : (
+                  <NoValue />
+                )
+              }
+            />
+          </Col>
+          <Col md={4} xs={12}>
+            <KeyValue
+              label={
+                <FormattedMessage id="ui-oa.publicationRequest.agreementStatus" />
+              }
+              value={agreement?.agreementStatus?.label ?? <NoValue />}
+            />
+          </Col>
+        </Row>
+      </>
+    );
+  };
+  return (
+    <Accordion
+      closedByDefault
+      displayWhenClosed={renderBadge(request?.agreement)}
+      displayWhenOpen={renderBadge(request?.agreement)}
+      label={<FormattedMessage id="ui-oa.publicationRequest.agreement" />}
+    >
+      {request?.agreement ? (
+        <Card
+          cardStyle="positive"
+          headerStart={
+            <AppIcon app="agreements" size="small">
+              {agreementLinkFunction ? (
+                <Link to={agreementLinkFunction(request?.agreement?.remoteId_object)}>
+                  <strong>{request?.agreement?.remoteId_object?.name}</strong>
+                </Link>
+              ) : (
+                <strong>{request?.agreement?.remoteId_object?.name}</strong>
+              )}
+            </AppIcon>
+          }
+        >
+          {renderAgreement(request?.agreement?.remoteId_object)}
+        </Card>
+      ) : (
+        renderEmpty()
+      )}
+    </Accordion>
+  );
+};
+
+Agreement.propTypes = propTypes;
+
+export default Agreement;
