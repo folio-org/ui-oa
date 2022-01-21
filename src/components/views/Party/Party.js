6677d21596556fc403dc8d79da41fa474cafb7c4
Jack Golding <jack.golding@k-int.com>
2022-01-21 16:40:47 +0000

    feat: Pane title
    Added pane title for slection of party, displaying as family name ordered

--- src/components/views/Party/Party.js
+++ src/components/views/Party/Party.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
+import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { AppIcon } from '@folio/stripes/core';
 
-import {
-  Pane,
-} from '@folio/stripes/components';
-
+import { Pane } from '@folio/stripes/components';
 
 const propTypes = {
   onClose: PropTypes.func.isRequired,
+  resource: PropTypes.object,
 };
 
-const Party = ({ onClose }) => {
+const Party = ({ resource: party, onClose }) => {
   return (
     <Pane
       appIcon={<AppIcon app="users" iconKey="app" size="small" />}
       defaultWidth="55%"
       dismissible
       onClose={onClose}
+      paneTitle={
+        <FormattedMessage
+          id="ui-oa.party.familyNameOrdered"
+          values={{
+            familyName: party?.familyName,
+            givenNames: party?.givenNames,
+          }}
+        />
+      }
     />
   );
 };
