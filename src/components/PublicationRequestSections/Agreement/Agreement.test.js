16966f4de323315e181df287c1746c370001bd8e
Jack Golding <jack.golding@k-int.com>
2022-01-24 14:54:52 +0000

    feat: Agreement Test
    Added test for agreement section of publication request view

---
+++ src/components/PublicationRequestSections/Agreement/Agreement.test.js
@@ -0,0 +1,60 @@
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { MemoryRouter } from 'react-router-dom';
+import { Accordion, KeyValue } from '@folio/stripes-testing';
+import translationsProperties from '../../../../test/helpers';
+import Agreement from './Agreement';
+import pbrAgreement from '../../../../test/resources/agreementResources';
+
+let renderComponent;
+
+describe('Agreement', () => {
+  describe('renders components with no initial values', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <Agreement />
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders Agreement Accordion component', async () => {
+      await Accordion('Agreement').exists();
+    });
+
+    test('renders Agreement card field', () => {
+      const { getByText } = renderComponent;
+      expect(
+        getByText('This request is not covered by an agreement')
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe('renders components with initial values', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <Agreement request={pbrAgreement} />
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders Agreement Accordion component', async () => {
+      await Accordion('Agreement').exists();
+    });
+
+    test('renders Agreement "Start date" KeyValue component', async () => {
+      await KeyValue('Start date').has({ value: '1/1/2021' });
+    });
+
+    test('renders Agreement "End date" KeyValue component', async () => {
+      await KeyValue('End date').has({ value: '12/31/2021' });
+    });
+
+    test('renders Agreement "Status" KeyValue component', async () => {
+      await KeyValue('Status').has({ value: 'Draft' });
+    });
+  });
+});
