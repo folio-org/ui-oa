bda3bbbaedf243f375eb74e6c1896aa7f81e2238
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2022-01-24 10:54:19 +0000

    feat: Party View (#80)
    * feat: Translations
    
    Added translations
    
    * feat: First render component
    
    Added first render component in search and filter pane, along with associated urls
    
    * feat: Routing for people
    
    Added routing for people search and filter
    
    * feat: Added more translations
    
    Added and reorganised translations for search and filter and view pane
    
    * feat: Framework for People route
    
    Added first version of PeopleRoute, still required to order MCL and change placeholder icon from oa to something else
    
    * revert: Revert TODO change
    
    Reverted change to TODO in PublicationRequestsRoute
    
    * feat: Journal Button
    
    Journal button now displayed but serves no purpose
    
    * chore: Updated TODO
    
    * feat: Added app icon
    
    Added appropriate app icon for people
    
    * feat: Reorganised Filters
    
    Reorganised filter files to accomadate for people and journal filters
    
    * feat: PeopleView Framework
    
    Added basic framework for people view pane
    
    * feat: Removed deprecated files
    
    Removed PublicationRequests as its been replaced by PublicationRequestsRoute
    
    * feat: Updated translation
    
    * refactor: Updated prop names
    
    Updated SASQRoute prop names for 1.7.0 update
    
    * feat: Initial sort state
    
    Added intial sort state for MCL
    
    * feat: Updated naming
    
    Changed naming for people to parties/party
    
    * feat: Implemented OAFilterHeaderComponent Framework
    
    Added FilterHeaderComponent framework
    
    * feat: Updated PublicationRequestRoute
    
    Added OAFilterHeaderComponent to PublicationRequestRoute
    
    * feat: Added OAFilter to PartiesRoute
    
    Fixed and added OAfilters to PartiesRoute
    
    * chore: Updated TODO
    
    * feat: Pane title
    
    Added pane title for slection of party, displaying as family name ordered
    
    * feat: Translations
    
    Added translations for party view
    
    * feat: Party info
    
    Added basic framework for party info in party view, still require ORCID iD, Phone and Mobile phone to be added to backend
    
    * chore: linting
    
    * fix: Botched merge
    
    Fixed leftover code from incorrect conflict resolution
    
    * fix: Mobile phone fix
    
    Mobile phone number now displays correctly in party view
    
    * chore: linting

--- src/components/views/Party/Party.js
+++ src/components/views/Party/Party.js
@@ -1,25 +1,44 @@
 import React from 'react';
 
+import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { AppIcon } from '@folio/stripes/core';
 
-import {
-  Pane,
-} from '@folio/stripes/components';
+import { Pane } from '@folio/stripes/components';
 
+import { PartyInfo } from '../../PartySections';
 
 const propTypes = {
   onClose: PropTypes.func.isRequired,
+  resource: PropTypes.object,
 };
 
-const Party = ({ onClose }) => {
+const Party = ({ resource: party, onClose }) => {
+  const getSectionProps = (name) => {
+    return {
+      id: `publication-request-section-${name}`,
+      party,
+    };
+  };
+
   return (
     <Pane
       appIcon={<AppIcon app="users" iconKey="app" size="small" />}
       defaultWidth="55%"
       dismissible
       onClose={onClose}
-    />
+      paneTitle={
+        <FormattedMessage
+          id="ui-oa.party.familyNameOrdered"
+          values={{
+            familyName: party?.familyName,
+            givenNames: party?.givenNames,
+          }}
+        />
+      }
+    >
+      <PartyInfo {...getSectionProps('partyInfo')} />
+    </Pane>
   );
 };
 
