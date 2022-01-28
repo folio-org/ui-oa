b47b25110a69d406a5663c009ee4ab3f02d16b8d
Jack Golding <jack.golding@k-int.com>
2022-01-28 15:35:38 +0000

    feat: RequestInfo test
    Added request info tests

---
+++ src/components/PublicationRequestSections/RequestInfo/RequestInfo.test.js
@@ -0,0 +1,58 @@
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { KeyValue } from '@folio/stripes-testing';
+import translationsProperties from '../../../../test/helpers';
+import RequestInfo from './RequestInfo';
+import { publicationRequest } from '../../../../test/resources/publicationRequestsResources';
+
+
+
+describe('RequestInfo', () => {
+  describe('renders components', () => {
+    beforeEach(() => {
+      renderWithIntl(
+        <RequestInfo />,
+        translationsProperties
+      );
+    });
+    test('renders Request number KeyValue', async () => {
+      await KeyValue('Request number').exists();
+    });
+
+    test('renders Request date KeyValue', async () => {
+      await KeyValue('Request date').exists();
+    });
+
+    test('renders Status KeyValue', async () => {
+      await KeyValue('Status').exists();
+    });
+
+    test('renders External request IDs KeyValue', async () => {
+      await KeyValue('External request IDs').exists();
+    });
+  });
+
+  describe('renders components with initial values', () => {
+    beforeEach(() => {
+      renderWithIntl(
+        <RequestInfo request={publicationRequest} />,
+        translationsProperties
+      );
+    });
+    test('renders Request number KeyValue with initial value', async () => {
+      await KeyValue('Request number').has({ value: '1' });
+    });
+
+    test('renders Request date KeyValue with initial value', async () => {
+      await KeyValue('Request date').has({ value: '1/1/2022' });
+    });
+
+    test('renders Status KeyValue with initial value', async () => {
+      await KeyValue('Status').has({ value: 'New' });
+    });
+
+    test('renders External request IDs KeyValue with initial value', async () => {
+      await KeyValue('External request IDs').has({ value: 'Test 1' });
+    });
+  });
+});
