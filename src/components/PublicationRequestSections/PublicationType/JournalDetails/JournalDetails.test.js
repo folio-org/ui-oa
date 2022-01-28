a0021678d77d7cac1c3a92704c78d9a58dacb22d
Jack Golding <jack.golding@k-int.com>
2022-01-28 16:11:40 +0000

    feat: JournalDetails test
    Added journal details tests

---
+++ src/components/PublicationRequestSections/PublicationType/JournalDetails/JournalDetails.test.js
@@ -0,0 +1,42 @@
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { KeyValue } from '@folio/stripes-testing';
+import translationsProperties from '../../../../../test/helpers';
+import JournalDetails from './JournalDetails';
+import journal from '../../../../../test/resources/journalResources';
+
+describe('JournalDetails', () => {
+  describe('renders components', () => {
+    beforeEach(() => {
+      renderWithIntl(<JournalDetails />, translationsProperties);
+    });
+    test('renders Title KeyValue', async () => {
+      await KeyValue('Title').exists();
+    });
+
+    test('renders ISSN (print) KeyValue', async () => {
+      await KeyValue('ISSN (print)').exists();
+    });
+
+    test('renders ISSN (electronic) KeyValue', async () => {
+      await KeyValue('ISSN (electronic)').exists();
+    });
+  });
+
+  describe('renders components with initial values', () => {
+    beforeEach(() => {
+      renderWithIntl(<JournalDetails journal={journal} />, translationsProperties);
+    });
+    test('renders Title KeyValue', async () => {
+      await KeyValue('Title').has({ value: 'Annals of Global Analysis and Geometry' });
+    });
+
+    test('renders ISSN (print) KeyValue', async () => {
+      await KeyValue('ISSN (print)').has({ value: '1572-9060' });
+    });
+
+    test('renders ISSN (electronic) KeyValue', async () => {
+      await KeyValue('ISSN (electronic)').has({ value: '0232-704X' });
+    });
+  });
+});
---
+++ test/resources/journalResources.js
@@ -0,0 +1,111 @@
+const journal = {
+
+    'id': 'd504c27d-1c99-4ec6-a1ba-22e319c33d79',
+    'title': 'Annals of Global Analysis and Geometry',
+    'instances': [
+        {
+            'id': 'f8a4747a-90fc-4cb8-8eba-0305828f7604',
+            'subType': {
+                'id': '2c9180b07e9e5db3017e9e61a153000c',
+                'value': 'print',
+                'label': 'Print',
+                'owner': {
+                    'id': '2c9180b07e9e5db3017e9e61a150000b',
+                    'desc': 'TitleInstance.SubType',
+                    'internal': true
+                }
+            },
+            'title': 'Annals of Global Analysis and Geometry',
+            'type': {
+                'id': '2c9180b07e9e5db3017e9e61a16d0010',
+                'value': 'serial',
+                'label': 'Serial',
+                'owner': {
+                    'id': '2c9180b07e9e5db3017e9e61a162000e',
+                    'desc': 'TitleInstance.Type',
+                    'internal': true
+                }
+            },
+            'publicationType': {
+                'id': '2c9180b07e9e5db3017e9e61a17e0013',
+                'value': 'journal',
+                'label': 'Journal',
+                'owner': {
+                    'id': '2c9180b07e9e5db3017e9e61a1730011',
+                    'desc': 'TitleInstance.PublicationType',
+                    'internal': false
+                }
+            },
+            'identifiers': [
+                {
+                    'title': {
+                        'id': 'f8a4747a-90fc-4cb8-8eba-0305828f7604'
+                    },
+                    'selected': true,
+                    'identifier': {
+                        'value': '1572-9060',
+                        'ns': {
+                            'value': 'issn'
+                        }
+                    }
+                }
+            ],
+            'work': {
+                'id': 'd504c27d-1c99-4ec6-a1ba-22e319c33d79'
+            }
+        },
+        {
+            'id': 'ae6f2b9d-047b-4ebf-81ba-ec93ce7ca380',
+            'subType': {
+                'id': '2c9180b07e9e5db3017e9e61a15d000d',
+                'value': 'electronic',
+                'label': 'Electronic',
+                'owner': {
+                    'id': '2c9180b07e9e5db3017e9e61a150000b',
+                    'desc': 'TitleInstance.SubType',
+                    'internal': true
+                }
+            },
+            'title': 'Annals of Global Analysis and Geometry',
+            'type': {
+                'id': '2c9180b07e9e5db3017e9e61a16d0010',
+                'value': 'serial',
+                'label': 'Serial',
+                'owner': {
+                    'id': '2c9180b07e9e5db3017e9e61a162000e',
+                    'desc': 'TitleInstance.Type',
+                    'internal': true
+                }
+            },
+            'publicationType': {
+                'id': '2c9180b07e9e5db3017e9e61a17e0013',
+                'value': 'journal',
+                'label': 'Journal',
+                'owner': {
+                    'id': '2c9180b07e9e5db3017e9e61a1730011',
+                    'desc': 'TitleInstance.PublicationType',
+                    'internal': false
+                }
+            },
+            'identifiers': [
+                {
+                    'title': {
+                        'id': 'ae6f2b9d-047b-4ebf-81ba-ec93ce7ca380'
+                    },
+                    'selected': true,
+                    'identifier': {
+                        'value': '0232-704X',
+                        'ns': {
+                            'value': 'issn'
+                        }
+                    }
+                }
+            ],
+            'work': {
+                'id': 'd504c27d-1c99-4ec6-a1ba-22e319c33d79'
+            }
+        }
+    ]
+};
+
+export default journal;
