9f4489fa23cf0567ba9860d941c7b3c8a678865e
Jack Golding <jack.golding@k-int.com>
2022-01-28 16:04:00 +0000

    feat: BookDetail Test
    Added book details tests

---
+++ src/components/PublicationRequestSections/PublicationType/BookDetails/BookDetails.test.js
@@ -0,0 +1,42 @@
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { KeyValue } from '@folio/stripes-testing';
+import translationsProperties from '../../../../../test/helpers';
+import BookDetails from './BookDetails';
+import book from '../../../../../test/resources/bookResources';
+
+
+
+describe('BookDetails', () => {
+  describe('renders components', () => {
+    beforeEach(() => {
+      renderWithIntl(
+        <BookDetails />,
+        translationsProperties
+      );
+    });
+    test('renders Place of publication KeyValue', async () => {
+      await KeyValue('Place of publication').exists();
+    });
+
+    test('renders Year of publication KeyValue', async () => {
+      await KeyValue('Year of publication').exists();
+    });
+  });
+
+  describe('renders components with initial values', () => {
+    beforeEach(() => {
+      renderWithIntl(
+        <BookDetails request={book} />,
+        translationsProperties
+      );
+    });
+    test('renders Place of publication KeyValue with initial value', async () => {
+      await KeyValue('Place of publication').has({ value: 'Test Place' });
+    });
+
+    test('renders Year of publication  KeyValue with initial value', async () => {
+      await KeyValue('Year of publication').has({ value: 'Test Year' });
+    });
+  });
+});
