bb4f0ac4027798b4ccffca59b82825b01cc4c856
Jack Golding <jack.golding@k-int.com>
2022-01-31 13:59:57 +0000

    feat: PartyInfo Tests
    Added tests to party info

---
+++ src/components/PartySections/PartyInfo/PartyInfo.test.js
@@ -0,0 +1,76 @@
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { KeyValue } from '@folio/stripes-testing';
+import translationsProperties from '../../../../test/helpers';
+import PartyInfo from './PartyInfo';
+import { party } from '../../../../test/resources/partyResources';
+
+describe('RequestInfo', () => {
+  describe('renders components', () => {
+    beforeEach(() => {
+      renderWithIntl(<PartyInfo />, translationsProperties);
+    });
+
+    test('renders Title KeyValue', async () => {
+      await KeyValue('Title').exists();
+    });
+
+    test('renders Family name KeyValue', async () => {
+      await KeyValue('Family name').exists();
+    });
+
+    test('renders Given name(s) KeyValue', async () => {
+      await KeyValue('Given name(s)').exists();
+    });
+
+    test('renders ORCID iD KeyValue', async () => {
+      await KeyValue('ORCID iD').exists();
+    });
+
+    test('renders Main email KeyValue', async () => {
+      await KeyValue('Main email address').exists();
+    });
+
+    test('renders Phone KeyValue', async () => {
+      await KeyValue('Phone').exists();
+    });
+
+    test('renders Mobile phone KeyValue', async () => {
+      await KeyValue('Mobile phone').exists();
+    });
+  });
+
+  describe('renders components with initial values', () => {
+    beforeEach(() => {
+      renderWithIntl(<PartyInfo party={party} />, translationsProperties);
+    });
+
+    test('renders Title KeyValue', async () => {
+      await KeyValue('Title').has({ value: 'Dr' });
+    });
+
+    test('renders Family name KeyValue', async () => {
+      await KeyValue('Family name').has({ value: 'Dempsey' });
+    });
+
+    test('renders Given name(s) KeyValue', async () => {
+      await KeyValue('Given name(s)').has({ value: 'Elijah' });
+    });
+
+    test('renders ORCID iD KeyValue', async () => {
+      await KeyValue('ORCID iD').has({ value: '0000-1111' });
+    });
+
+    test('renders Main email KeyValue', async () => {
+      await KeyValue('Main email address').has({ value: 'Elijah_Dempsey6074@brety.org' });
+    });
+
+    test('renders Phone KeyValue', async () => {
+      await KeyValue('Phone').has({ value: '012345678' });
+    });
+
+    test('renders Mobile phone KeyValue', async () => {
+      await KeyValue('Mobile phone').has({ value: '012345678' });
+    });
+  });
+});
