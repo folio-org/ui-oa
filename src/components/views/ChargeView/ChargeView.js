a459663c6eaef87f42bb61c2d2f4ccb415514a66
Jack Golding <jack.golding@k-int.com>
2022-02-09 17:20:53 +0000

    feat: Charge Information Displayed
    Charge information is now displayed upon selecting a charge from the charges MCL, also added a find function to grab the ID of selected charge and grab that from the publication request, this will need to be reworked later

--- src/components/views/ChargeView/ChargeView.js
+++ src/components/views/ChargeView/ChargeView.js
@@ -3,19 +3,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { AppIcon } from '@folio/stripes/core';
-import { useHistory } from 'react-router-dom';
-import { Pane } from '@folio/stripes/components';
+import { useHistory, useLocation } from 'react-router-dom';
+import { Pane, Row, Col, KeyValue } from '@folio/stripes/components';
 
 const propTypes = {
   resource: PropTypes.object,
 };
 
 const ChargeView = ({ resource: request }) => {
+  const location = useLocation();
   const history = useHistory();
 
-   const handleClose = () => {
+  const chargeId = location.pathname.split('/').pop();
+  const charge = request?.charges?.find((e) => e?.id === chargeId);
+
+  const handleClose = () => {
     history.push(`/oa/publicationRequests/${request.id}`);
-   };
+  };
 
   return (
     <Pane
@@ -24,7 +28,42 @@ const ChargeView = ({ resource: request }) => {
       dismissible
       onClose={handleClose}
       paneTitle={<FormattedMessage id="ui-oa.charge.title" />}
-    />
+    >
+      <Row>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.amount" />}
+            value={charge?.amount?.value + ' ' + charge?.amount?.baseCurrency}
+          />
+        </Col>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.discount" />}
+            value={charge?.discount + ' ' + charge?.amount?.baseCurrency}
+          />
+        </Col>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.exchangeRate" />}
+            value={charge?.exchangeRate?.toCurrency}
+          />
+        </Col>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.coefficient" />}
+            value={charge?.exchangeRate?.coefficient}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.description" />}
+            value={charge?.description}
+          />
+        </Col>
+      </Row>
+    </Pane>
   );
 };
 
