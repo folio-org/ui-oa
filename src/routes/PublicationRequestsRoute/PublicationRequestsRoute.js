dfe779b0c7e11333e0efd527eeacdb7fbdbcd885
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-03-09 13:06:39 +0000

    chore: Import fix
    Removed unnecessary import of Route

--- src/routes/PublicationRequestsRoute/PublicationRequestsRoute.js
+++ src/routes/PublicationRequestsRoute/PublicationRequestsRoute.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { FormattedMessage } from 'react-intl';
-import { Route } from 'react-router-dom';
 
 import { AppIcon, IfPermission } from '@folio/stripes/core';
 
