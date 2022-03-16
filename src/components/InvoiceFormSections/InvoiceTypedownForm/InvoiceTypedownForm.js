eef53f5bbab0fa55d9b3f485395a6fd290650e79
Jack Golding <jack.golding@k-int.com>
2022-03-16 15:11:24 +0000

    feat: Invoice Typedown Form
    Added invoice typedown form along with related card

---
+++ src/components/InvoiceFormSections/InvoiceTypedownForm/InvoiceTypedownForm.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { FormattedMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+import { Field, useForm, useFormState } from 'react-final-form';
+
+import {
+  Button,
+  Layout,
+  Label,
+  IconButton,
+  Card,
+} from '@folio/stripes/components';
+import { AppIcon } from '@folio/stripes/core';
+import { Typedown } from '@k-int/stripes-kint-components';
+
+import { InvoiceModal } from '../../Modals';
+import InvoiceInfo from '../../InvoiceSections';
+
+const propTypes = {
+  charge: PropTypes.object,
+};
+
+const InvoiceTypedownForm = ({ charge }) => {
+  const { values } = useFormState();
+  const { change } = useForm();
+  const [showInvoiceModal, setShowInvoiceModal] = useState(false);
+
+  const handleInvoiceChange = (invoice) => {
+    change('invoice', invoice);
+    setShowInvoiceModal(false);
+  };
+
+  const renderFooter = () => {
+    return (
+      <Layout className="textCentered">
+        <Button
+          buttonStyle="primary"
+          marginBottom0
+          onClick={() => setShowInvoiceModal(true)}
+        >
+          <FormattedMessage id="ui-oa.charge.invoice.newInvoice" />
+        </Button>
+      </Layout>
+    );
+  };
+  return (
+    <>
+      <>
+        <Label>
+          <FormattedMessage id="ui-oa.charge.invoice.addInvoice" />
+        </Label>
+        <Field
+          component={Typedown}
+          name="invoice"
+          renderFooter={renderFooter}
+        />
+        {values.invoice && JSON.stringify(values.invoice)}
+        {values.invoice && (
+          <Card
+            cardStyle="positive"
+            headerEnd={
+              <IconButton icon="trash" onClick={() => handleInvoiceChange()} />
+            }
+            headerStart={
+              <AppIcon size="small">
+                <strong>{values?.invoice?.number}</strong>
+              </AppIcon>
+            }
+          >
+            <InvoiceInfo invoice={values?.invoice} />
+          </Card>
+        )}
+      </>
+      <InvoiceModal
+        charge={charge}
+        handleInvoiceChange={handleInvoiceChange}
+        setShowModal={setShowInvoiceModal}
+        showModal={showInvoiceModal}
+      />
+    </>
+  );
+};
+
+InvoiceTypedownForm.propTypes = propTypes;
+
+export default InvoiceTypedownForm;
---
+++ src/components/InvoiceSections/InvoiceInfo/InvoiceInfo.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+
+import { FormattedMessage } from 'react-intl';
+
+import { Col, KeyValue, Row } from '@folio/stripes/components';
+
+const propTypes = {
+  invoice: PropTypes.object,
+};
+
+const InvoiceInfo = ({ invoice }) => {
+  return (
+    <>
+      <Row>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.invoice.invoiceDate" />}
+            value={invoice?.date}
+          />
+        </Col>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.invoice.vendorOrganisation" />}
+            value={invoice.vendorOrganisation}
+          />
+        </Col>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.invoice.paymentMethod" />}
+            value={invoice.paymentMethod}
+          />
+        </Col>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.invoice.batchGroup" />}
+            value={invoice.batchGroup}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.currency" />}
+            value={invoice?.currency}
+          />
+        </Col>
+        <Col xs={3}>
+          <KeyValue
+            label={<FormattedMessage id="ui-oa.charge.exchangeRate" />}
+            value={invoice.exchangeRate}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+InvoiceInfo.propTypes = propTypes;
+
+export default InvoiceInfo;
