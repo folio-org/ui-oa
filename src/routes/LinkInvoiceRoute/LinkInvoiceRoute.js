48aacedacf4e0f9bd44b84758f966c3e679e9780
Jack Golding <jack.golding@k-int.com>
2022-03-23 11:01:57 +0000

    feat: LinkInvoice Submission
    INvoice is now correctly linked to charge upon submission

--- src/routes/LinkInvoiceRoute/LinkInvoiceRoute.js
+++ src/routes/LinkInvoiceRoute/LinkInvoiceRoute.js
@@ -2,14 +2,18 @@ import { Form } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 import { useHistory, useParams } from 'react-router-dom';
 import { useOkapiKy } from '@folio/stripes/core';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 import LinkInvoiceForm from '../../components/views/LinkInvoiceForm';
 import urls from '../../util/urls';
+import useOARefdata from '../../util/useOARefdata';
 
 const LinkInvoiceRoute = () => {
   const history = useHistory();
   const ky = useOkapiKy();
   const { prId, chId } = useParams();
+  const invoicedRefData = useOARefdata('Charge.ChargeStatus').find(
+    (e) => e.value === 'invoiced'
+  );
 
   const handleClose = () => {
     history.push(urls.publicationRequestChargeView(prId, chId));
@@ -20,9 +24,27 @@ const LinkInvoiceRoute = () => {
     () => ky(`oa/publicationRequest/${prId}`).json()
   );
 
-  const charge = request?.charges?.find(e => e.id === chId);
+  const { mutateAsync: linkInvoice } = useMutation(
+    ['ui-oa', 'LinkInvoiceRoute', 'linkInvoice'],
+    (data) => ky.put(`oa/publicationRequest/${prId}`, { json: data }).then(() => {
+        handleClose();
+      })
+  );
 
-  const submitInvoice = () => {
+  const charge = request?.charges?.find((e) => e.id === chId);
+
+  const submitInvoice = (values) => {
+    const submitValues = {
+      charges: [
+        {
+          ...charge,
+          invoiceReference: values?.selectedInvoice?.id,
+          invoiceLineItemReference: values?.invoiceLine?.id,
+          chargeStatus: invoicedRefData,
+        },
+      ],
+    };
+    linkInvoice(submitValues);
   };
 
   return (
