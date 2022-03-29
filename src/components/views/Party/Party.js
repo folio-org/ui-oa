119d7ff5d15ce0862c4d6bb8fd67e59f0241462a
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2022-03-29 16:12:55 +0100

    refactor: Related requests fetch (#147)
    * refactor: Related requests fetch
    
    Related requests no longer pulls all requests then filters, now only querys for requests with matching corresponding author ids
    
    UIOA-92
    
    * fix: caching issue

--- src/components/views/Party/Party.js
+++ src/components/views/Party/Party.js
@@ -20,13 +20,10 @@ const Party = ({ resource: party, onClose, queryProps: { isLoading } }) => {
   const history = useHistory();
   const params = useParams();
 
+  // Filter publication requests in which the corresponding author matches the current party
   const { data: publicationRequests } = useQuery(
-    ['ui-oa', 'party', 'publicationRequests'],
-    () => ky('oa/publicationRequest').json()
-  );
-
-  const relatedRequests = publicationRequests?.filter(
-    (request) => request?.correspondingAuthor?.partyOwner?.id === party?.id
+    ['ui-oa', 'party', 'publicationRequests', party.id],
+    () => ky(`oa/publicationRequest?filters=correspondingAuthor.partyOwner.id==${party.id}`).json()
   );
 
   const getSectionProps = (name) => {
@@ -72,8 +69,8 @@ const Party = ({ resource: party, onClose, queryProps: { isLoading } }) => {
       }
     >
       <PartyInfo {...getSectionProps('partyInfo')} />
-      {!!relatedRequests?.length && (
-        <RelatedRequests requests={relatedRequests} />
+      {!!publicationRequests?.length && (
+        <RelatedRequests requests={publicationRequests} />
       )}
     </Pane>
   );
