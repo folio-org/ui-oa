f883c420f3c58f2cc3c087a83679b8e262d4f031
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2022-04-01 17:53:28 +0100

    refactor: Refactored modals (#154)
    Refactored invoice, party and journal modals to use FormModal

--- src/components/Modals/JournalModal/JournalModal.js
+++ src/components/Modals/JournalModal/JournalModal.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
-import { Form } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 import { FormattedMessage } from 'react-intl';
 import { useMutation } from 'react-query';
 
 import { useOkapiKy } from '@folio/stripes/core';
-import { Button, Modal, ModalFooter, Loading, Layout } from '@folio/stripes/components';
+import { Loading, Layout } from '@folio/stripes/components';
+import { FormModal } from '@k-int/stripes-kint-components';
 
 import JournalInfoForm from '../../JournalFormSections/JournalInfoForm';
 import css from './JournalModal.css';
@@ -25,72 +25,41 @@ const JournalModal = ({ showModal, setShowModal, handleJournalChange }) => {
 
   const { mutateAsync: postJournal, isLoading } = useMutation(
     ['ui-oa', 'JournalModal', 'postJournal'],
-    (data) => ky.post('oa/works/citation', { json: data }).json().then((res) => {
-      handleJournalChange(res);
-      handleClose();
-    })
+    (data) => ky.post('oa/works/citation', { json: data }).json()
   );
 
-  const submitJournal = (values) => {
-    const submitValues = { ...values, 'type':'serial' };
-    postJournal(submitValues);
-  };
-
-  const renderModalFooter = (handleSubmit, formRestart) => {
-    return (
-      <ModalFooter>
-        <Button
-          buttonStyle="primary"
-          id="journal-modal-save-button"
-          onClick={() => {
-            handleSubmit().then(formRestart);
-          }}
-          type="submit"
-        >
-          <FormattedMessage id="stripes-components.saveAndClose" />
-        </Button>
-        <Button
-          buttonStyle="default"
-          id="journal-modal-cancel-button"
-          onClick={() => setShowModal(false)}
-        >
-          <FormattedMessage id="stripes-components.cancel" />
-        </Button>
-      </ModalFooter>
-    );
+  const submitJournal = (values, form) => {
+    const submitValues = { ...values, type: 'serial' };
+    postJournal(submitValues).then((res) => {
+      handleJournalChange(res);
+      handleClose();
+      form.restart();
+    });
   };
 
   return (
-    <Form
+    <FormModal
       // Setting initial values of type to serial instead of select field
       initialValues={{
         title: '',
         instances: [{ ids: [{ ns: '', id: '' }], subType: '' }],
       }}
+      modalProps={{
+        onClose: handleClose,
+        open: showModal,
+        label: <FormattedMessage id="ui-oa.journal.newJournal" />,
+      }}
       mutators={arrayMutators}
       onSubmit={submitJournal}
-      render={({ handleSubmit, form: { restart: formRestart } }) => (
-        <form onSubmit={handleSubmit}>
-          <Modal
-            dismissible
-            footer={renderModalFooter(handleSubmit, formRestart)}
-            label={<FormattedMessage id="ui-oa.journal.newJournal" />}
-            onClose={() => setShowModal(false)}
-            open={showModal}
-          >
-            {isLoading ? (
-              <Layout
-                className={css.spinnerStyle}
-              >
-                <Loading size="large" />
-              </Layout>
-            ) : (
-              <JournalInfoForm />
-            )}
-          </Modal>
-        </form>
+    >
+      {isLoading ? (
+        <Layout className={css.spinnerStyle}>
+          <Loading size="large" />
+        </Layout>
+      ) : (
+        <JournalInfoForm />
       )}
-    />
+    </FormModal>
   );
 };
 
--- src/components/Modals/PartyModal/PartyModal.js
+++ src/components/Modals/PartyModal/PartyModal.js
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types';
-import { Form } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 import { useMutation } from 'react-query';
 import { FormattedMessage } from 'react-intl';
 
-import { Button, Modal, ModalFooter } from '@folio/stripes/components';
 import { useOkapiKy } from '@folio/stripes/core';
+import { FormModal } from '@k-int/stripes-kint-components';
 
 import { PartyInfoForm } from '../../PartyFormSections';
 
@@ -24,54 +23,29 @@ const PartyModal = ({ showModal, setShowModal, handlePartyChange }) => {
 
   const { mutateAsync: postParty } = useMutation(
     ['ui-oa', 'PartyModal', 'postParty'],
-    (data) => ky.post('oa/party', { json: data }).json().then((res) => {
-        handlePartyChange(res);
-        handleClose();
-      })
+    (data) => ky.post('oa/party', { json: data }).json()
   );
 
-  const renderModalFooter = (handleSubmit, formRestart) => {
-    return (
-      <ModalFooter>
-        <Button
-          buttonStyle="primary"
-          id="party-modal-save-button"
-          onClick={() => {
-            handleSubmit().then(formRestart);
-          }}
-          type="submit"
-        >
-          <FormattedMessage id="stripes-components.saveAndClose" />
-        </Button>
-        <Button
-          buttonStyle="default"
-          id="party-modal-cancel-button"
-          onClick={() => setShowModal(false)}
-        >
-          <FormattedMessage id="stripes-components.cancel" />
-        </Button>
-      </ModalFooter>
-    );
+  const submitParty = (values, form) => {
+    postParty(values).then((res) => {
+      handlePartyChange(res);
+      handleClose();
+      form.restart();
+    });
   };
 
   return (
-    <Form
+    <FormModal
+      modalProps={{
+        onClose: handleClose,
+        open: showModal,
+        label: <FormattedMessage id="ui-oa.party.newPerson" />,
+      }}
       mutators={arrayMutators}
-      onSubmit={postParty}
-      render={({ handleSubmit, form: { restart: formRestart } }) => (
-        <form onSubmit={handleSubmit}>
-          <Modal
-            dismissible
-            footer={renderModalFooter(handleSubmit, formRestart)}
-            label={<FormattedMessage id="ui-oa.party.newPerson" />}
-            onClose={() => setShowModal(false)}
-            open={showModal}
-          >
-            <PartyInfoForm />
-          </Modal>
-        </form>
-      )}
-    />
+      onSubmit={submitParty}
+    >
+      <PartyInfoForm />
+    </FormModal>
   );
 };
 
