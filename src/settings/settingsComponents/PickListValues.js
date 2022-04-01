dea3999957f4f2c7b5948cfb697ecdfd594891e0
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-04-01 11:21:03 +0100

    fix: Pick list settings
    Added onClose to pickList settings page to avoid crash on dismiss pane

--- src/settings/settingsComponents/PickListValues.js
+++ src/settings/settingsComponents/PickListValues.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useHistory } from 'react-router-dom';
 
 import { Pane, Select } from '@folio/stripes/components';
 import { EditableRefdataList } from '@k-int/stripes-kint-components';
@@ -9,12 +10,14 @@ import { REFDATA_ENDPOINT } from '../../constants';
 const PickListValues = () => {
  const rdcOptions = useOARefdata()?.map(rdv => ({ value: rdv.desc, label: rdv.desc }));
  const [selectedPickList, setSelectedPickList] = useState('');
+ const history = useHistory();
 
   return (
     <Pane
       defaultWidth="fill"
       dismissible
       id="edit-refdata-requestStatus"
+      onClose={() => history.push('/settings/oa')}
       paneTitle={<FormattedMessage id="ui-oa.settings.refdata.picklistValues" />}
     >
       <Select
