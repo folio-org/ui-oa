453b273ecf598a2acaf92d9868983cd9b6dfb438
Jack Golding <jack.golding@k-int.com>
2022-04-11 13:47:57 +0100

    feat: Street Address Form
    Added singular street address form to party form

---
+++ src/components/PartyFormSections/StreetAddress/StreetAddress.js
@@ -0,0 +1,90 @@
+import { FormattedMessage } from 'react-intl';
+import { Field } from 'react-final-form';
+
+import { Col, Row, TextField, Card } from '@folio/stripes/components';
+import { requiredValidator } from '@folio/stripes-erm-components';
+
+const StreetAddress = () => {
+  return (
+    <>
+      <Card
+        headerStart={
+          <strong>
+            <FormattedMessage
+              id="ui-oa.party.streetAddress.index"
+              values={{ index: '' }}
+            />
+          </strong>
+        }
+      >
+        <Row>
+          <Col xs={3}>
+            <Field
+              component={TextField}
+              label={<FormattedMessage id="ui-oa.party.streetAddress.name" />}
+              name="streetAddress.address.name"
+            />
+          </Col>
+          <Col xs={3}>
+            <Field
+              component={TextField}
+              label={
+                <FormattedMessage id="ui-oa.party.streetAddress.addressLineOne" />
+              }
+              name="streetAddress.address.addressLineOne"
+            />
+          </Col>
+          <Col xs={3}>
+            <Field
+              component={TextField}
+              label={
+                <FormattedMessage id="ui-oa.party.streetAddress.addressLineTwo" />
+              }
+              name="streetAddress.address.addressLineTwo"
+            />
+          </Col>
+          <Col xs={3}>
+            <Field
+              component={TextField}
+              label={<FormattedMessage id="ui-oa.party.streetAddress.city" />}
+              name="streetAddress.address.city"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={3}>
+            <Field
+              component={TextField}
+              label={<FormattedMessage id="ui-oa.party.streetAddress.region" />}
+              name="streetAddress.address.region"
+            />
+          </Col>
+          <Col xs={3}>
+            <Field
+              component={TextField}
+              label={
+                <FormattedMessage id="ui-oa.party.streetAddress.country" />
+              }
+              name="streetAddress.address.country"
+              required
+              validate={requiredValidator}
+            />
+          </Col>
+          <Col xs={3}>
+            <Field
+              component={TextField}
+              label={
+                <FormattedMessage id="ui-oa.party.streetAddress.postalCode" />
+              }
+              name="streetAddress.address.postalCode"
+              required
+              validate={requiredValidator}
+            />
+          </Col>
+        </Row>
+      </Card>
+    </>
+  );
+};
+
+export default StreetAddress;
--- src/components/PartyFormSections/index.js
+++ src/components/PartyFormSections/index.js
@@ -1,2 +1,4 @@
 export { default as OtherEmailsFieldArray } from './OtherEmailsFieldArray';
 export { default as PartyInfoForm } from './PartyInfoForm/PartyInfoForm';
+export { default as StreetAddressesFieldArray } from './StreetAddressesFieldArray';
+export { default as StreetAddress } from './StreetAddress';
