ce059507a7f2aea6c6a4405874e2ac0142a343fc
Jack Golding <jack.golding@k-int.com>
2022-05-17 15:14:23 +0100

    feat: Namespace select conditional
    Added the feature that removes options from the select fields when they have be used in other select fields of the same type

--- src/components/JournalFormSections/FieldArrays/InstanceIdentifiersFieldArray/InstanceIdentifiersFieldArray.js
+++ src/components/JournalFormSections/FieldArrays/InstanceIdentifiersFieldArray/InstanceIdentifiersFieldArray.js
@@ -1,7 +1,9 @@
+import { useState, useEffect } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 
-import { Field } from 'react-final-form';
+import { Field, useFormState } from 'react-final-form';
 import { FieldArray } from 'react-final-form-arrays';
 
 import {
@@ -21,89 +23,112 @@ const propTypes = {
 };
 
 const InstanceIdentifiersFieldArray = ({ instanceId }) => {
+  const { values } = useFormState();
   const intl = useIntl();
+  const [optionsInUse, setOptionsInUse] = useState([]);
+
+  // Sets the options that are currently being used by getting the namespace value of all identifiers
+  useEffect(() => {
+    setOptionsInUse(
+      get(values, `${instanceId}.ids`)
+        ?.map((i) => i?.ns)
+        .filter((x) => !!x)
+    );
+  }, [instanceId, values]);
+
+  const dataOptions = [
+    { value: ' ', label: ' ' },
+    {
+      value: 'issn',
+      label: intl.formatMessage({
+        id: 'ui-oa.journal.instance.identifier.ns.issn',
+      }),
+    },
+    {
+      value: 'ezb',
+      label: intl.formatMessage({
+        id: 'ui-oa.journal.instance.identifier.ns.ezb',
+      }),
+    },
+    {
+      value: 'zdb',
+      label: intl.formatMessage({
+        id: 'ui-oa.journal.instance.identifier.ns.zdb',
+      }),
+    },
+  ];
+
   const renderIdentifier = (fields) => {
     return (
       <>
         {/* This correctly renders the initial values of a new journal form */}
         {fields.length < 1 && fields.update()}
-        {fields.map((identifierId, index) => (
-          <div
-            key={identifierId}
-            data-testid={`InstanceIdentifiersFieldArray[${index}]`}
-          >
-            <Row>
-              <Col xs={4}>
-                <Field
-                  component={Select}
-                  dataOptions={[
-                    { value: '', label: '' },
-                    {
-                      value: 'issn',
-                      label: intl.formatMessage({
-                        id: 'ui-oa.journal.instance.identifier.ns.issn',
-                      }),
-                    },
-                    {
-                      value: 'ezb',
-                      label: intl.formatMessage({
-                        id: 'ui-oa.journal.instance.identifier.ns.ezb',
-                      }),
-                    },
-                    {
-                      value: 'zdb',
-                      label: intl.formatMessage({
-                        id: 'ui-oa.journal.instance.identifier.ns.zdb',
-                      }),
-                    },
-                  ]}
-                  label={
-                    <FormattedMessage id="ui-oa.journal.instance.identifier.nameSpace" />
-                  }
-                  name={`${identifierId}.ns`}
-                  required
-                  validate={requiredValidator}
-                />
-              </Col>
-              <Col xs={4}>
-                <Field
-                  component={TextField}
-                  label={
-                    <FormattedMessage id="ui-oa.journal.instance.identifier.id" />
-                  }
-                  maxLength={MAX_CHAR_LONG}
-                  name={`${identifierId}.id`}
-                  required
-                  validate={requiredValidator}
-                />
-              </Col>
-              {fields.length !== 1 && (
+        {fields?.map((identifierId, index) => {
+          // This removes the options that are currently in use from the total data option pool
+          // This excludes the one that has been selected for the current field and the empty string value, so the field can be cleared
+          const availableOptions = dataOptions.filter(
+            (data) => !optionsInUse?.includes(data.value) ||
+              data.value === get(values, identifierId).ns ||
+              data.value === ' '
+          );
+          return (
+            <div
+              key={identifierId}
+              data-testid={`InstanceIdentifiersFieldArray[${index}]`}
+            >
+              <Row>
                 <Col xs={4}>
-                  <Tooltip
-                    id={`${identifierId}-trash-button-tooltip`}
-                    text={
-                      <FormattedMessage
-                        id="ui-oa.journal.deleteIdentifier"
-                        values={{ index: index + 1 }}
-                      />
+                  <Field
+                    component={Select}
+                    dataOptions={availableOptions}
+                    label={
+                      <FormattedMessage id="ui-oa.journal.instance.identifier.nameSpace" />
                     }
-                  >
-                    {({ ref, ariaIds }) => (
-                      <IconButton
-                        ref={ref}
-                        aria-describedby={ariaIds.sub}
-                        aria-labelledby={ariaIds.text}
-                        icon="trash"
-                        onClick={() => fields.remove(index)}
-                        style={{ 'padding-top': '25px' }}
-                      />
-                    )}
-                  </Tooltip>
+                    name={`${identifierId}.ns`}
+                    required
+                    validate={requiredValidator}
+                  />
                 </Col>
-              )}
-            </Row>
-          </div>
-        ))}
+                <Col xs={4}>
+                  <Field
+                    component={TextField}
+                    label={
+                      <FormattedMessage id="ui-oa.journal.instance.identifier.id" />
+                    }
+                    maxLength={MAX_CHAR_LONG}
+                    name={`${identifierId}.id`}
+                    required
+                    validate={requiredValidator}
+                  />
+                </Col>
+                {fields.length !== 1 && (
+                  <Col xs={4}>
+                    <Tooltip
+                      id={`${identifierId}-trash-button-tooltip`}
+                      text={
+                        <FormattedMessage
+                          id="ui-oa.journal.deleteIdentifier"
+                          values={{ index: index + 1 }}
+                        />
+                      }
+                    >
+                      {({ ref, ariaIds }) => (
+                        <IconButton
+                          ref={ref}
+                          aria-describedby={ariaIds.sub}
+                          aria-labelledby={ariaIds.text}
+                          icon="trash"
+                          onClick={() => fields.remove(index)}
+                          style={{ 'padding-top': '25px' }}
+                        />
+                      )}
+                    </Tooltip>
+                  </Col>
+                )}
+              </Row>
+            </div>
+          );
+        })}
       </>
     );
   };
