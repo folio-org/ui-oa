c7631f84acb55bf9a1b283f7d00bc6eec1f7a71f
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2022-05-19 14:35:18 +0100

    feat: Party info tweaks (#197)
    * feat: Street address validators
    
    Removedvalidators for zip and country within street address form
    
    * feat: Party info tweaks
    
    Tweaked party info form and view, removed the card to store street addresses as well as various styling changes with regards to various pieces of information
    
    * feat: Other email links
    
    Upon viewing a party, other email addresses are now displayed as links

--- src/components/PartyFormSections/StreetAddress/StreetAddress.js
+++ src/components/PartyFormSections/StreetAddress/StreetAddress.js
@@ -1,104 +1,81 @@
 import { FormattedMessage } from 'react-intl';
 import { Field } from 'react-final-form';
 
-import { Col, Row, TextField, Card, Label } from '@folio/stripes/components';
-import { requiredValidator } from '@folio/stripes-erm-components';
+import { Col, Row, TextField, Label } from '@folio/stripes/components';
 import { MAX_CHAR_LONG } from '../../../constants/config';
 
 const StreetAddress = () => {
   return (
     <>
       <Label>
-        <FormattedMessage id="ui-oa.party.streetAddresses" />
+        <FormattedMessage
+          id="ui-oa.party.streetAddress.index"
+          values={{ index: '' }}
+        />
       </Label>
-      <Card
-        headerStart={
-          <strong>
-            <FormattedMessage
-              id="ui-oa.party.streetAddress.index"
-              values={{ index: '' }}
-            />
-          </strong>
-        }
-        roundedBorder
-      >
-        <Row>
-          <Col xs={3}>
-            <Field
-              component={TextField}
-              label={<FormattedMessage id="ui-oa.party.streetAddress.name" />}
-              name="streetAddress.address.name"
-              parse={(v) => v}
-            />
-          </Col>
-          <Col xs={3}>
-            <Field
-              component={TextField}
-              label={
-                <FormattedMessage id="ui-oa.party.streetAddress.addressLineOne" />
-              }
-              maxLength={MAX_CHAR_LONG}
-              name="streetAddress.address.addressLineOne"
-              parse={(v) => v}
-            />
-          </Col>
-          <Col xs={3}>
-            <Field
-              component={TextField}
-              label={
-                <FormattedMessage id="ui-oa.party.streetAddress.addressLineTwo" />
-              }
-              maxLength={MAX_CHAR_LONG}
-              name="streetAddress.address.addressLineTwo"
-              parse={(v) => v}
-            />
-          </Col>
-          <Col xs={3}>
-            <Field
-              component={TextField}
-              label={<FormattedMessage id="ui-oa.party.streetAddress.city" />}
-              maxLength={MAX_CHAR_LONG}
-              name="streetAddress.address.city"
-              parse={(v) => v}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={3}>
-            <Field
-              component={TextField}
-              label={<FormattedMessage id="ui-oa.party.streetAddress.region" />}
-              maxLength={MAX_CHAR_LONG}
-              name="streetAddress.address.region"
-              parse={(v) => v}
-            />
-          </Col>
-          <Col xs={3}>
-            <Field
-              component={TextField}
-              label={
-                <FormattedMessage id="ui-oa.party.streetAddress.country" />
-              }
-              maxLength={MAX_CHAR_LONG}
-              name="streetAddress.address.country"
-              required
-              validate={requiredValidator}
-            />
-          </Col>
-          <Col xs={3}>
-            <Field
-              component={TextField}
-              label={
-                <FormattedMessage id="ui-oa.party.streetAddress.postalCode" />
-              }
-              maxLength={MAX_CHAR_LONG}
-              name="streetAddress.address.postalCode"
-              required
-              validate={requiredValidator}
-            />
-          </Col>
-        </Row>
-      </Card>
+      <br />
+      <Row>
+        <Col xs={3}>
+          <Field
+            component={TextField}
+            label={
+              <FormattedMessage id="ui-oa.party.streetAddress.addressLineOne" />
+            }
+            maxLength={MAX_CHAR_LONG}
+            name="streetAddress.address.addressLineOne"
+            parse={(v) => v}
+          />
+        </Col>
+        <Col xs={3}>
+          <Field
+            component={TextField}
+            label={
+              <FormattedMessage id="ui-oa.party.streetAddress.addressLineTwo" />
+            }
+            maxLength={MAX_CHAR_LONG}
+            name="streetAddress.address.addressLineTwo"
+            parse={(v) => v}
+          />
+        </Col>
+        <Col xs={3}>
+          <Field
+            component={TextField}
+            label={<FormattedMessage id="ui-oa.party.streetAddress.city" />}
+            maxLength={MAX_CHAR_LONG}
+            name="streetAddress.address.city"
+            parse={(v) => v}
+          />
+        </Col>
+        <Col xs={3}>
+          <Field
+            component={TextField}
+            label={<FormattedMessage id="ui-oa.party.streetAddress.region" />}
+            maxLength={MAX_CHAR_LONG}
+            name="streetAddress.address.region"
+            parse={(v) => v}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          <Field
+            component={TextField}
+            label={<FormattedMessage id="ui-oa.party.streetAddress.country" />}
+            maxLength={MAX_CHAR_LONG}
+            name="streetAddress.address.country"
+          />
+        </Col>
+        <Col xs={3}>
+          <Field
+            component={TextField}
+            label={
+              <FormattedMessage id="ui-oa.party.streetAddress.postalCode" />
+            }
+            maxLength={MAX_CHAR_LONG}
+            name="streetAddress.address.postalCode"
+          />
+        </Col>
+      </Row>
     </>
   );
 };
