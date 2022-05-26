3383d37dd84db91c8e3b8425be175e582674d2a2
Jack Golding <jack.golding@k-int.com>
2022-05-26 11:44:35 +0100

    feat: Keyboard shortcuts modal
    Added the keyboard shortcuts modal to the index of OA, default keybind is ctrl + alt + k

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React, { Suspense } from 'react';
+import { useState, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import {
+  CommandList,
+  importShortcuts,
+  HasCommand,
+  KeyboardShortcutsModal,
+  checkScope,
+} from '@folio/stripes/components';
+
 import Settings from './settings';
 
 import {
@@ -22,6 +31,7 @@ import {
 } from './routes';
 
 const App = (props) => {
+  const [showShortcutModal, setShowShortcutModal] = useState(false);
   const {
     actAs,
     match: { path },
@@ -35,57 +45,101 @@ const App = (props) => {
     );
   }
 
+  const keyboardShortcuts = importShortcuts([
+    'new',
+    'edit',
+    'save',
+    'expandAllSections',
+    'collapseAllSections',
+    'expandOrCollapseAccordion',
+    'search',
+    'openShortcutModal',
+  ]);
+
+  const changeShortcutsModal = () => {
+    setShowShortcutModal(!showShortcutModal);
+  };
+
+  const shortcuts = [
+    {
+      name: 'openShortcutModal',
+      handler: changeShortcutsModal,
+    },
+  ];
+
   return (
-    <Suspense fallback={null}>
-      <Switch>
-        <Route
-          component={PublicationRequestCreateRoute}
-          path={`${path}/publicationRequests/create`}
+    <>
+      <CommandList commands={keyboardShortcuts}>
+        <HasCommand
+          commands={shortcuts}
+          isWithinScope={checkScope}
+          scope={document.body}
+        >
+          <Suspense fallback={null}>
+            <Switch>
+              <Route
+                component={PublicationRequestCreateRoute}
+                path={`${path}/publicationRequests/create`}
+              />
+              <Route
+                component={PublicationRequestEditRoute}
+                path={`${path}/publicationRequests/:id/edit`}
+              />
+              <Route
+                component={CorrespondenceCreateRoute}
+                path={`${path}/publicationRequests/:id/correspondence/create`}
+              />
+              <Route
+                component={CorrespondenceEditRoute}
+                path={`${path}/publicationRequests/:prId/correspondence/:cId/edit`}
+              />
+              <Route
+                component={LinkInvoiceRoute}
+                path={`${path}/publicationRequests/:prId/charge/:chId/linkInvoice`}
+              />
+              <Route
+                component={ChargeCreateRoute}
+                path={`${path}/publicationRequests/:id/charge/create`}
+              />
+              <Route
+                component={ChargeEditRoute}
+                path={`${path}/publicationRequests/:prId/charge/:chId/edit`}
+              />
+              <Route
+                component={JournalEditRoute}
+                path={`${path}/journals/:id/edit`}
+              />
+              <Route
+                component={PartyCreateRoute}
+                path={`${path}/people/create`}
+              />
+              <Route
+                component={PartyEditRoute}
+                path={`${path}/people/:id/edit`}
+              />
+              <PartiesRoute path={`${path}/people`} />
+              <JournalsRoute path={`${path}/journals`} />
+              <PublicationRequestsRoute path={`${path}/publicationRequests`}>
+                <Route
+                  component={ChargeRoute}
+                  path={`${path}/publicationRequests/:prId/charge/:chId`}
+                />
+                <Route
+                  component={CorrespondenceViewRoute}
+                  path={`${path}/publicationRequests/:prId/correspondence/:cId`}
+                />
+              </PublicationRequestsRoute>
+            </Switch>
+          </Suspense>
+        </HasCommand>
+      </CommandList>
+      {showShortcutModal && (
+        <KeyboardShortcutsModal
+          allCommands={keyboardShortcuts}
+          onClose={changeShortcutsModal}
         />
-        <Route
-          component={PublicationRequestEditRoute}
-          path={`${path}/publicationRequests/:id/edit`}
-        />
-        <Route
-          component={CorrespondenceCreateRoute}
-          path={`${path}/publicationRequests/:id/correspondence/create`}
-        />
-        <Route
-          component={CorrespondenceEditRoute}
-          path={`${path}/publicationRequests/:prId/correspondence/:cId/edit`}
-        />
-        <Route
-          component={LinkInvoiceRoute}
-          path={`${path}/publicationRequests/:prId/charge/:chId/linkInvoice`}
-        />
-        <Route
-          component={ChargeCreateRoute}
-          path={`${path}/publicationRequests/:id/charge/create`}
-        />
-        <Route
-          component={ChargeEditRoute}
-          path={`${path}/publicationRequests/:prId/charge/:chId/edit`}
-        />
-        <Route
-          component={JournalEditRoute}
-          path={`${path}/journals/:id/edit`}
-        />
-        <Route component={PartyCreateRoute} path={`${path}/people/create`} />
-        <Route component={PartyEditRoute} path={`${path}/people/:id/edit`} />
-        <PartiesRoute path={`${path}/people`} />
-        <JournalsRoute path={`${path}/journals`} />
-        <PublicationRequestsRoute path={`${path}/publicationRequests`}>
-          <Route
-            component={ChargeRoute}
-            path={`${path}/publicationRequests/:prId/charge/:chId`}
-          />
-          <Route
-            component={CorrespondenceViewRoute}
-            path={`${path}/publicationRequests/:prId/correspondence/:cId`}
-          />
-        </PublicationRequestsRoute>
-      </Switch>
-    </Suspense>
+      )}
+    </>
   );
 };
 
