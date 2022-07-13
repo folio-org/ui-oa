9d5ee55fc660774d9d4c7d08b69827906de83869
Jack Golding <jack.golding@k-int.com>
2022-07-13 14:22:55 +0100

    feat: Required/Not required dropdown
    Added dropdown action menu for the handling of setting the status of a checklist item to be required/not required

--- src/components/Checklist/ChecklistForm.js
+++ src/components/Checklist/ChecklistForm.js
@@ -5,11 +5,15 @@ import {
   InfoPopover,
   KeyValue,
   IconButton,
+  Dropdown,
+  DropdownMenu,
+  Button,
+  Icon,
 } from '@folio/stripes/components';
 import { IconSelect } from '@k-int/stripes-kint-components';
 import { orderBy } from 'lodash';
 import PropTypes from 'prop-types';
-import { Field } from 'react-final-form';
+import { Field, useForm } from 'react-final-form';
 import { FormattedMessage } from 'react-intl';
 import ChecklistNotesModal from './ChecklistNotesModal';
 
@@ -20,6 +24,7 @@ const propTypes = {
   checklist: PropTypes.object,
 };
 const ChecklistForm = ({ checklist }) => {
+  const { change } = useForm();
   const [showNotesModal, setShowNotesModal] = useState(false);
 
   const buttonOptions = [
@@ -47,6 +52,35 @@ const ChecklistForm = ({ checklist }) => {
       {checklistItemNames.map((name) => {
         const item = checklist?.items[name];
         const sortedNotes = orderBy(item.notes, 'dateCreated', 'desc');
+
+        const renderMenu = ({ onToggle }) => {
+          return (
+            <DropdownMenu>
+              <Button
+                buttonStyle="dropdownItem"
+                onClick={() => {
+                  change(`items[${name}].status`, 'required');
+                  onToggle();
+                }}
+              >
+                <Icon icon="eye-open">
+                  <FormattedMessage id="ui-oa.checklist.required" />
+                </Icon>
+              </Button>
+              <Button
+                buttonStyle="dropdownItem"
+                onClick={() => {
+                  change(`items[${name}].status`, 'not_required');
+                  onToggle();
+                }}
+              >
+                <Icon icon="eye-closed">
+                  <FormattedMessage id="ui-oa.checklist.notRequired" />
+                </Icon>
+              </Button>
+            </DropdownMenu>
+          );
+        };
         return (
           <div key={item?.id} className={css.container}>
             <Row>
@@ -54,7 +88,29 @@ const ChecklistForm = ({ checklist }) => {
                 <KeyValue
                   label={
                     <>
-                      {item.label} <InfoPopover content={item?.description} />
+                      {item.label}
+                      <Dropdown
+                        hasPadding
+                        renderMenu={renderMenu}
+                        renderTrigger={({
+                          open,
+                          onToggle,
+                          triggerRef,
+                          keyHandler,
+                          getTriggerProps,
+                        }) => (
+                          <IconButton
+                            ref={triggerRef}
+                            icon={open ? 'caret-up' : 'caret-down'}
+                            marginBottom0
+                            onClick={onToggle}
+                            onKeyDown={keyHandler}
+                            type="button"
+                            {...getTriggerProps()}
+                          />
+                        )}
+                      />
+                      <InfoPopover content={item?.description} />
                     </>
                   }
                   value={
