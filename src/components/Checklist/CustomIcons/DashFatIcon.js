470eb745fc6322d0dfc4b210c8ddd5c5f57c7d4f
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-08-04 11:26:41 +0100

    style: Icons
    Work done on the custom icons and styling
    
    UIOA-143

--- src/components/Checklist/CustomIcons/DashFatIcon.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/style-prop-object */
-const DashFatIcon = (props) => (
-  <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M9.017 15.813h13.966" />
-  </svg>
-);
-
-export default DashFatIcon;
