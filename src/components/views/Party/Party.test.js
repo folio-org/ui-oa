e3a6c88ffaa956bac99fcb2ed8e482e69155387b
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2022-11-08 12:54:53 +0000

    feat: Retrospective OA (#350)
    * refactor: Publication request form expand/collapse button
    
    Changed location of publication request collapse/expand all button to top of first accordion
    
    * feat: Retrospective OA checkbox
    
    Added checkbox for retrospective OA to publication request form
    
    * feat: Retrospective OA tooltip
    
    added infopopover toretrospective OA field within publication request form
    
    * feat: Retrospective OA view pane
    
    Added retrospective OA to publication request view pane
    
    * feat: Retrospective OA filter
    
    Aded filter to publication request filters to filter by whether a request is retrospectively open access

--- src/components/views/Party/Party.test.js
+++ src/components/views/Party/Party.test.js
@@ -1,7 +1,6 @@
 import '@folio/stripes-erm-components/test/jest/__mock__';
 
 import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
-import { Button } from '@folio/stripes-testing';
 
 import translationsProperties from '../../../../test/helpers/translationsProperties';
 import Party from './Party';
