c86e0cead4c461c9a4253cfbf4a30a152dc2eba7
Jack Golding <jack.golding@k-int.com>
2022-11-08 10:31:59 +0000

    feat: Retrospective OA checkbox
    Added checkbox for retrospective OA to publication request form

--- src/components/PublicationRequestFormSections/RequestInfoForm/RequestInfoForm.js
+++ src/components/PublicationRequestFormSections/RequestInfoForm/RequestInfoForm.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 
-import {
-  Field
-} from 'react-final-form';
+import { Field, useForm, useFormState } from 'react-final-form';
 
 import {
   Col,
@@ -12,11 +10,11 @@ import {
   KeyValue,
   Row,
   Select,
+  Checkbox,
+  Label,
 } from '@folio/stripes/components';
 
-import {
-  requiredValidator,
-} from '@folio/stripes-erm-components';
+import { requiredValidator } from '@folio/stripes-erm-components';
 
 import { useOARefdata, selectifyRefdata } from '../../../util';
 
@@ -27,13 +25,18 @@ const propTypes = {
 };
 
 const RequestInfoForm = ({ request }) => {
+  const { values } = useFormState();
+  const { change } = useForm();
   const requestStatusValues = useOARefdata('PublicationRequest.RequestStatus');
+
   return (
     <>
       <Row start="xs">
         <Col xs={3}>
           <KeyValue
-            label={<FormattedMessage id="ui-oa.publicationRequest.requestNumber" />}
+            label={
+              <FormattedMessage id="ui-oa.publicationRequest.requestNumber" />
+            }
             value={request?.requestNumber}
           />
         </Col>
@@ -43,7 +46,9 @@ const RequestInfoForm = ({ request }) => {
             backendDateStandard="YYYY-MM-DD"
             component={Datepicker}
             id="request-date"
-            label={<FormattedMessage id="ui-oa.publicationRequest.requestDate" />}
+            label={
+              <FormattedMessage id="ui-oa.publicationRequest.requestDate" />
+            }
             name="requestDate"
             required
             timeZone="UTC"
@@ -53,7 +58,10 @@ const RequestInfoForm = ({ request }) => {
         <Col xs={3}>
           <Field
             component={Select}
-            dataOptions={[{ value: '', label: '' }, ...selectifyRefdata(requestStatusValues)]}
+            dataOptions={[
+              { value: '', label: '' },
+              ...selectifyRefdata(requestStatusValues),
+            ]}
             id="request-status"
             label={<FormattedMessage id="ui-oa.publicationRequest.status" />}
             name="requestStatus.id"
@@ -61,7 +69,20 @@ const RequestInfoForm = ({ request }) => {
             validate={requiredValidator}
           />
         </Col>
-        <Col xs={3} />
+        <Col xs={3}>
+          <Label>
+            <FormattedMessage id="ui-oa.publicationRequest.retrospectiveOA" />
+          </Label>
+          <Field
+            checked={values.withoutAgreement}
+            component={Checkbox}
+            name="retrospectiveOA"
+            onChange={(e) => {
+              change('retrospectiveOA', e.target.checked);
+            }}
+            type="checkbox"
+          />
+        </Col>
       </Row>
       <Row>
         <Col xs>
