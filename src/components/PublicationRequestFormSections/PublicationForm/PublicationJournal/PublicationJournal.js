3ef77257b6758420a091194c2db73df40767ae00
Jack Golding <jack.golding@k-int.com>
2023-02-07 15:49:29 +0000

    fix: ISSN highlight
    ISSNs are now correctly highlighted when searched in typedown

--- src/components/PublicationRequestFormSections/PublicationForm/PublicationJournal/PublicationJournal.js
+++ src/components/PublicationRequestFormSections/PublicationForm/PublicationJournal/PublicationJournal.js
@@ -77,8 +77,8 @@ const PublicationJournal = () => {
         id="ui-oa.publicationJournal.typedown"
         values={{
           title: highlightString(input, work.title),
-          printIssn: printIssn?.value || '',
-          electronicIssn: electronicIssn?.value || '',
+          printIssn: highlightString(input, printIssn?.value || ''),
+          electronicIssn: highlightString(input, electronicIssn?.value || ''),
         }}
       />
     );
