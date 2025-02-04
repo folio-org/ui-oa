d222d41f66fd120624d6da709c884dd6061bc628
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-02-04 14:15:04 +0000

    refactor: refactored useMutateChecklistItemDefinitions to use useMutateGeneric (POC)

--- src/hooks/useMutateChecklistItemDefinitions.js
+++ src/hooks/useMutateChecklistItemDefinitions.js
@@ -1,72 +1,45 @@
-import { useMutation, useQueryClient } from 'react-query';
-
-import { useOkapiKy } from '@folio/stripes/core';
+import { useQueryClient } from 'react-query';
+import noop from 'lodash/noop';
+import { useMutateGeneric } from '@k-int/stripes-kint-components';
 
 import { CHECKLIST_ITEM_DEFINITIONS_ENDPOINT } from '../constants/endpoints';
 
 const useMutateChecklistItemDefinitions = ({
-  afterQueryCalls,
-  catchQueryCalls,
-  queryParams,
-  returnQueryObject = {
-    post: false,
-    put: false,
-    delete: false
-  }
+   afterQueryCalls: {
+     delete: afterQueryDelete = noop,
+     post: afterQueryPost = noop,
+     put: afterQueryPut = noop,
+   } = {},
+  ...mutateGenericProps
 }) => {
-  const returnObj = {};
   const queryClient = useQueryClient();
-
-  const ky = useOkapiKy();
-
   const invalidateChecklistItemDefinition = () => queryClient.invalidateQueries(['OA', 'ChecklistItemDefinitions']);
 
-  const deleteQueryObject = useMutation(
-    ['OA', 'ChecklistItemDefinitions', 'Delete'],
-    async (id) => ky.delete(`${CHECKLIST_ITEM_DEFINITIONS_ENDPOINT}/${id}`).json()
-      .then(afterQueryCalls?.delete)
-      .then(() => invalidateChecklistItemDefinition())
-      .catch(catchQueryCalls?.delete),
-    queryParams?.delete
-  );
-
-  const putQueryObject = useMutation(
-    ['OA', 'ChecklistItemDefinitions', 'Put'],
-    async (data) => ky.put(`${CHECKLIST_ITEM_DEFINITIONS_ENDPOINT}/${data.id}`, { json: data }).json()
-      .then(afterQueryCalls?.put)
-      .then(() => invalidateChecklistItemDefinition())
-      .catch(catchQueryCalls?.put),
-    queryParams?.put
-  );
-
-  const postQueryObject = useMutation(
-    ['OA', 'ChecklistItemDefinitions', 'Post'],
-    async (data) => ky.post(`${CHECKLIST_ITEM_DEFINITIONS_ENDPOINT}`, { json: data }).json()
-      .then(afterQueryCalls?.post)
-      .then(() => invalidateChecklistItemDefinition())
-      .catch(catchQueryCalls?.post),
-    queryParams?.post
-  );
-
-  if (returnQueryObject?.delete) {
-    returnObj.delete = deleteQueryObject;
-  } else {
-    returnObj.delete = deleteQueryObject.mutateAsync;
-  }
-
-  if (returnQueryObject?.put) {
-    returnObj.put = putQueryObject;
-  } else {
-    returnObj.put = putQueryObject.mutateAsync;
-  }
-
-  if (returnQueryObject?.post) {
-    returnObj.post = postQueryObject;
-  } else {
-    returnObj.post = postQueryObject.mutateAsync;
-  }
-
-  return returnObj;
+  return useMutateGeneric({
+    afterQueryCalls: {
+      delete: (res) => {
+        invalidateChecklistItemDefinition()
+          .then(() => {
+            afterQueryDelete(res);
+          });
+      },
+      post: (res) => {
+        invalidateChecklistItemDefinition()
+          .then(() => {
+            afterQueryPost(res);
+          });
+      },
+      put: (res) => {
+        invalidateChecklistItemDefinition()
+          .then(() => {
+            afterQueryPut(res);
+          });
+      },
+    },
+    endpoint: CHECKLIST_ITEM_DEFINITIONS_ENDPOINT,
+    queryKey: ['OA', 'ChecklistItemDefinitions'],
+    ...mutateGenericProps
+  });
 };
 
 export default useMutateChecklistItemDefinitions;
